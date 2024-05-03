5de9b4bcae2660fb6081b0f20a80d40e7dff44a8
matthieu-rmh <razafintsalama.rmh@gmail.com>
2024-05-03 15:24:50 +0200

    add small note about strict mode usage in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    reactStrictMode: false
+    /* 
+        When set to true, the parameter below 
+        allow strict mode which makes the callback inside 
+        the useEffect hooks to get called twice 
+    */
+    reactStrictMode: true
 };
 
 export default nextConfig;
