856a6cbcaa4a4c5488fcb32d07c6a333c592e022
matthieu-rmh <razafintsalama.rmh@gmail.com>
2024-05-17 13:52:51 +0300

    create odoo branch to test fetching odoo data starting from session_id from authentication

--- app/dashboard/items/page.tsx
+++ app/dashboard/items/page.tsx
@@ -2,11 +2,15 @@ import { getItems} from "@app/lib/data";
 import {Item} from "@app/lib/definitions";
 import { isFirstLoggedIn} from "@app/lib/actions";
 import ItemsTable from "@app/components/items-table";
+import { odooAuthenticate } from "@app/lib/odoo";
 
 export default async function Page(){
     let isFLoggedIn = await isFirstLoggedIn();
 
     let items:Item[] = await getItems();
+
+    let odooAuthenticationResBody = await odooAuthenticate();
+    console.log( odooAuthenticationResBody );
 
     return(
         <div>
@@ -14,6 +18,9 @@ export default async function Page(){
                 <h1 className="border-b border-gray-300 text-3xl">Items</h1>
             </div>
             <ItemsTable items={items} isFLoggedIn={isFLoggedIn}/>
+            <div>
+                {odooAuthenticationResBody}
+            </div>
         </div>
     );
 }
---
+++ app/lib/odoo.ts
@@ -0,0 +1,39 @@
+export const odooAuthenticate = async () => {
+    /* 
+        Get session_id cookie from the authentication from the odoo instance
+    */
+
+    // body of the authentication request with my Odoo credentials
+    const params = {
+        params: 
+        {
+            db: "main-bte",
+            login: "admin",
+            password: "admin"
+        }
+    }
+
+    const res = await fetch('http://localhost:8069/web/session/authenticate',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(params)
+    });
+
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data');
+    }
+
+    // Get all cookies from the  response's headers
+
+    let  cookies = res.headers.get("Set-Cookie");
+
+    // parse the header  to get the session_id
+    let session_id = cookies?.split(";")[0].split("=")[1];
+   
+    // console.log(result);
+    return session_id;
+    
+}
