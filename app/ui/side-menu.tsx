f38713a71bb19eaf36461cc328cbd1eb8b875f65
matthieu-rmh <razafintsalama.rmh@gmail.com>
2024-05-21 16:12:28 +0300

    add dark mode to side-menu

--- app/ui/side-menu.tsx
+++ app/ui/side-menu.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export default function SideMenu(){
     return(
-        <aside className="bg-gray-200 w-auto p-6 border-r border-gray-300">
+        <aside className="bg-gray-200 dark:bg-gray-800 w-auto p-6 border-r border-gray-300 ease-out duration-300">
             <nav>
                 <ul className="divide-y divide-gray-500">
-                    <li className="h-14 flex items-center"><Link href="/dashboard/items" className="font-medium text-gray-900 hover:text-gray-700">Items</Link></li>
-                    <li className="h-14 flex items-center"><Link href="#" className="font-medium text-gray-900 hover:text-gray-700">Users</Link></li>
-                    <li className="h-14 flex items-center"><Link href="#" className="font-medium text-gray-900 hover:text-gray-700">Settings</Link></li>
+                    <li className="h-14 flex items-center"><Link href="/dashboard/items" className="font-medium text-gray-900 dark:text-white hover:text-gray-700">Items</Link></li>
+                    <li className="h-14 flex items-center"><Link href="#" className="font-medium text-gray-900 dark:text-white hover:text-gray-700">Users</Link></li>
+                    <li className="h-14 flex items-center"><Link href="#" className="font-medium text-gray-900 dark:text-white hover:text-gray-700">Settings</Link></li>
                 </ul>
             </nav>
          </aside>
