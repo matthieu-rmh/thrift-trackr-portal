c716e0e98ed2b206beb19342fba6c77493d322e1
matthieu-rmh <razafintsalama.rmh@gmail.com>
2024-05-22 17:42:17 +0300

    set a decent homepage'

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { playfairDisplay } from "./ui/fonts";
+
 export default function Home() {
 
   return (
-    <div>
-      Let's go
+    <div className="w-full flex justify-center items-center bg-gradient-to-r from-slate-300 to-slate-600 dark:from-slate-900 dark:to-slate-500 ease-out duration-300">
+      <div className="flex flex-col w-1/3 h-1/3 mx-auto my-auto justify-center items-center">
+        <h1 className={`${playfairDisplay.className} text-5xl my-10`}>THRIFT TRACKR.</h1>
+        <a href="/login" className="flex justify-center items-center w-28 h-16 bg-indigo-600 hover:bg-indigo-700 border border-transparent rounded-md my-10"><p className="text-white text-center text-lg">Log In</p></a>
+
+      </div>
     </div>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Playfair_Display, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const playfairDisplay = Playfair_Display({ subsets: ['latin']});
+// export const lusitana = Lusitana({
+//     weight: ['400', '700'],
+//     subsets: ['latin'],
+//   });
